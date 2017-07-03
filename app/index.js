b2d7d71f1fc7b2202f6c0e58c1689560fe3a6151
Josh Lee <joshswl@gmail.com>
2017-07-03 12:55:21 -0500

    finish

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ function toggleOpen() {
   this.classList.toggle('open');
 }
 
-function toggleActive() {
+function toggleActive(e) {
   console.log(e.propertyName);
   if (e.propertyName.includes('flex')) {
     this.classList.toggle('open-active');
